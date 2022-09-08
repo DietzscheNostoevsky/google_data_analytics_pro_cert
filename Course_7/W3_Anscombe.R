8c7585883483107cc20dfb72b87ceb9d184781e6
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-09-08 11:58:37 +0530

    datasauRus

--- Course_7/W3_Anscombe.R
+++ Course_7/W3_Anscombe.R
@@ -7,3 +7,6 @@ quartet %>%
   summarise(mean(x),sd(x) , mean(y),sd(y), cor(x,y) )
 
 ggplot(quartet, aes(x,y)) + geom_point() + geom_smooth(method=lm, se= FALSE) + facet_wrap(~set)
+
+install.packages("datasauRus")
+library(datasauRus)
