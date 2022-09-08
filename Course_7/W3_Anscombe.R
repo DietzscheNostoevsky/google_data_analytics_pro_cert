6b36df39975058e0e242207151a16abf71cf1835
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-09-08 11:56:39 +0530

    plot

--- Course_7/W3_Anscombe.R
+++ Course_7/W3_Anscombe.R
@@ -5,3 +5,5 @@ View(quartet)
 quartet %>%
   group_by(set) %>% 
   summarise(mean(x),sd(x) , mean(y),sd(y), cor(x,y) )
+
+ggplot(quartet, aes(x,y)) + geom_point() + geom_smooth(method=lm, se= FALSE) + facet_wrap(~set)
