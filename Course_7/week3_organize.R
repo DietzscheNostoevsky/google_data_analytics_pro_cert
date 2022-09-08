a59aaa068f58aaee79aedf8ccdbece25d612926d
DietzscheNostoevsky <96907475+DietzscheNostoevsky@users.noreply.github.com>
2022-09-08 10:07:28 +0530

    Update week3_organize.R

--- Course_7/week3_organize.R
+++ Course_7/week3_organize.R
@@ -1,2 +1,4 @@
 penguins %>% group_by(island) %>% drop_na() %>% summarise(mean_bill_length_mm = max(bill_length_mm))
 
+penguins %>% group_by(species , island) %>% drop_na() %>% summarise(max_bl = max(bill_length_mm) , mean_bl = mean(bill_length_mm))
+
